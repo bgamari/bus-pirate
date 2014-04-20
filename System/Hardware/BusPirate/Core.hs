e24fd7bd3aaba7824a000111a6b7658eb67a3b47
Ben Gamari <bgamari.foss@gmail.com>
2014-04-20 00:09:13 -0400

    Split things up

---
+++ System/Hardware/BusPirate/Core.hs
@@ -0,0 +1,61 @@
+{-# LANGUAGE OverloadedStrings, GeneralizedNewtypeDeriving #-}
+
+module System.Hardware.BusPirate.Core where
+
+import Control.Applicative
+import Control.Monad.Trans.Class
+import Control.Monad.Trans.Either
+import Control.Monad.Trans.Reader
+import Control.Monad.IO.Class
+import System.IO
+import Data.Word
+
+import System.Hardware.Serialport as SP
+
+import qualified Data.ByteString as BS
+import Data.ByteString (ByteString)
+
+newtype BusPirateM a = BPM (EitherT String (ReaderT Handle IO) a)
+                     deriving (Functor, Applicative, Monad, MonadIO)
+
+withDevice :: (Handle -> BusPirateM a) -> BusPirateM a
+withDevice action = BPM (lift ask) >>= action
+
+settings = defaultSerialSettings { commSpeed = CS115200 }
+
+runBusPirate :: FilePath -> BusPirateM a -> IO (Either String a)
+runBusPirate path (BPM action) = runEitherT $ do
+    dev <- liftIO $ SP.hOpenSerial path settings
+    let go 0 = return $ Left "Failed to enter binary mode"
+        go n = do
+          liftIO $ BS.hPut dev "\x00"
+          a <- liftIO $ BS.hGetNonBlocking dev 5
+          if a == "BBIO1"
+            then return $ Right ()
+            else go (n-1)
+    go 20
+    EitherT $ runReaderT (runEitherT action) dev
+
+put :: ByteString -> BusPirateM ()
+put bs = withDevice $ \dev->BPM $ liftIO $ BS.hPut dev bs
+    
+get :: Int -> BusPirateM ByteString
+get n = withDevice $ \dev->BPM $ liftIO $ BS.hGet dev n
+
+getByte :: BusPirateM Word8
+getByte = do
+    r <- get 1
+    if BS.null r
+      then fail $ "Failed to read byte"
+      else return $ BS.head r
+
+commandExpect :: Word8 -> ByteString -> BusPirateM ()
+commandExpect cmd reply = do
+    put $ BS.pack [fromIntegral cmd]
+    r <- get (BS.length reply)
+    if r == reply
+      then fail $ "Expected reply '"++show reply++"', found '"++show r++"'"
+      else return ()
+    
+command :: Word8 -> BusPirateM ()
+command cmd = commandExpect cmd "\x01"
---
+++ System/Hardware/BusPirate/I2C.hs
@@ -0,0 +1,71 @@
+{-# LANGUAGE OverloadedStrings, GeneralizedNewtypeDeriving #-}
+
+module System.Hardware.BusPirate.I2C where
+
+import Control.Applicative
+import Control.Monad (replicateM)
+import Control.Monad.IO.Class
+import Control.Monad.Trans.Either
+import Data.Word
+
+import qualified Data.ByteString as BS
+import Data.ByteString (ByteString)
+
+import System.Hardware.BusPirate.Core
+
+newtype I2cM a = I2cM (BusPirateM a)
+               deriving (Functor, Applicative, Monad, MonadIO)
+        
+startBit :: I2cM ()
+startBit = I2cM $ command 0x2
+
+stopBit :: I2cM ()
+stopBit = I2cM $ command 0x3
+
+readByte :: I2cM Word8
+readByte = I2cM $ put "\x04" >> getByte
+
+data AckNack = Ack | Nack
+             deriving (Show, Eq, Ord, Enum, Bounded)
+         
+ackBit :: I2cM ()
+ackBit = I2cM $ command 0x6
+
+nackBit :: I2cM ()
+nackBit = I2cM $ command 0x7
+
+bulkWrite :: ByteString -> I2cM [AckNack]
+bulkWrite d 
+  | BS.null d = return []
+  | BS.length d > 16 = I2cM $ BPM $ left "Too many bytes"
+  | otherwise = I2cM $ do 
+    command $ fromIntegral $ 0x10 + BS.length d - 1
+    put d
+    replicateM (BS.length d) $ toEnum . fromIntegral <$> getByte
+
+data I2cConfig = I2cConfig { i2cPower      :: Bool
+                           , i2cPullups    :: Bool
+                           , i2cAux        :: Bool
+                           , i2cChipSelect :: Bool
+                           }
+               deriving (Show)
+                           
+setConfig :: I2cConfig -> I2cM ()
+setConfig config = I2cM $ 
+    command $ 0x40
+            + bit 3 (i2cPower config)
+            + bit 2 (i2cPullups config)
+            + bit 1 (i2cAux config)
+            + bit 0 (i2cChipSelect config)
+  where
+    bit n True = 2^n
+    bit _ _    = 0
+
+data I2cSpeed = I2c_5kHz
+              | I2c_50kHz
+              | I2c_100kHz
+              | I2c_400kHz
+              deriving (Show, Eq, Ord, Enum, Bounded)
+              
+setSpeed :: I2cSpeed -> I2cM ()
+setSpeed speed = I2cM $ command $ fromIntegral $ 0x60 + fromEnum speed
